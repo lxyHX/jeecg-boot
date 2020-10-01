2aa76d1b33da6086cec62418ea5319c4da680b4a
lxyHX <527138445@qq.com>
2020-10-01 11:03:01 +0800

    xx

---
+++ ant-design-vue-jeecg/src/views/faultManagement/FaultConfigList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+AAAA
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FaultConfigList",
+      data: () => ({
+      })
+    }
+</script>
+
+<style scoped>
+
+</style>
